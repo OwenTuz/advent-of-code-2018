9258cdad81be6c012db8e2389a3ea18110c2cd02
Owen Tuz <owen@segfault.re>
2019-06-25 08:54:58 +0100

    Day 5 part 2

--- src/bin/day5/main.rs
+++ src/bin/day5/main.rs
@@ -44,7 +44,36 @@ fn react_chain(chain: &str) -> String {
 
 }
 
+#[test]
+fn test_find_best_reaction(){
+    assert_eq!(4, find_best_reaction("dabAcCaCBAcCcaDA"));
+}
+fn find_best_reaction(chain: &str) -> usize {
+    // Slight cheat - no need to scan through the input and build a character
+    // map as we trust our input and know it will be in the set [A-Za-z]
+    let lowercase_alphabet = (b'a'..b'z').filter_map(|c| {
+        let c = c as char;
+        if c.is_alphabetic() {
+            Some(c)
+        } else { None }
+    });
+    let mut shortest_found: Option<usize> = None;
+    for x in lowercase_alphabet {
+        let new_chain: String = chain.chars()
+                                     .filter(|c| {
+                                        c != &x && c != &x.to_ascii_uppercase()
+                                     }).collect();
+        let length = react_chain(&new_chain).chars().count();
+        if shortest_found == None || length < shortest_found.unwrap() {
+            shortest_found = Some(length)
+        }
+    }
+    shortest_found.unwrap()
+}
+
+
 fn main() {
     let input = include_str!("input").trim();
     println!("Part1: Answer is {:?}", react_chain(input).chars().count());
+    println!("Part2: Answer is {:?}", find_best_reaction(input));
 }
